c7290cf2f4c2b57e9acf2cbc9a9943b7cefe32b6
Arjun Mukerji <stormandstress@gmail.com>
2015-12-10 12:22:23 -0800

    working get_cond_timepoints() function that returns the timepoints associated with the specified conditions/EV files. Includes code to specify conditions for emotion and language tasks. includes sanity checks to make sure no timepoints are assigned to multiple conditions.

--- parse_evs.py
+++ parse_evs.py
@@ -24,7 +24,7 @@ def get_epifile(sub, task):
 	epi_dir = get_epidir(sub, task)
 	return os.path.join(epi_dir, '%s.nii.gz'%task_str)
 
-def get_cond_timeseries(sub, task, conds):
+def get_cond_timepoints(sub, task, conds):
 	'''Reads the onset/EV files for the specified subject and task, and splits them according to a dict of conditions.
 
 	sub	:Subject number, as a string
@@ -41,7 +41,7 @@ def get_cond_timeseries(sub, task, conds):
 
 	TR = 0.72 # seconds; figure out where to embed or obtain this knowledge. should be correct for HCP.
 
-	cond_timeseries = dict()
+	cond_timepoints = dict()
 
 	# check that all the filenames given in conds exist in the dir
 	for condition in conds.keys():
@@ -55,27 +55,30 @@ def get_cond_timeseries(sub, task, conds):
 		for evfile in cond_evfiles:
 			f = open(os.path.join(ev_dir,evfile),'r')
 			for line in f:
-				# handle each event listing. these are all allowed to overlap with each other (long events relative to ITI)
+				# handle each event listing. these are all allowed to overlap with each other WITHIN a condition (long events relative to ITI)
 				# the format is onset	duration	amplitude, we only care about the first two
 				# eventually we just want the set of timepoints of the events and no others
 				[onset, duration, amp] = [float(p) for p in line.split()]
-				first_idx = int(math.floor(onset/TR)) # not using ceil here because if an event began at TR 10.5, we want vol 11 onwards, which has index 10 as python is 0-based
+				first_idx = int(math.floor(onset/TR)) #if an event began at TR 10.5, we want vol 11 onwards, which has index 10
 				duration_TR = int(math.ceil(duration/TR))
 				last_idx = first_idx + duration_TR # inclusive! we want this index as well
 				print (onset, onset/TR, duration, duration_TR, first_idx, last_idx)
 				idxs = range(first_idx, last_idx+1)
+				# NOTE: I believe we should account for BOLD lag by ignoring the first n timepoints of each event
+				# as a first pass/prototype this is not implemented yet
+				# make sure the range is correct
 				assert(min(idxs)==first_idx and max(idxs)==last_idx)
 				cond_tps.update(idxs)
 
-		cond_timeseries[condition] = cond_tps
+		cond_timepoints[condition] = cond_tps
 	else: # will be executed when the 'for condition in conds.keys()' loop terminates by exhaustion, ie all conditions are checked and no errors were thrown
 		# sanity checks
 		# make sure no timepoints are shared with other existing conditions! this is a hard constraint.
-		assert(len(set.intersection(*cond_timeseries.values()))==0)
+		assert(len(set.intersection(*cond_timepoints.values()))==0)
 		# any further checks go here
-		# return the sorted and labeled timeseries
-		cond_timeseries_sorted = {condition: sorted(timepoints) for condition, timepoints in cond_timeseries.items()}
-		return cond_timeseries_sorted
+		# return the sorted (ordered) list of timepoints comprising each condition
+		cond_timepoints= {condition: sorted(timepoints) for condition, timepoints in cond_timepoints.items()}
+		return cond_timepoints
 
 
 tasks = ['EMOTION', 'GAMBLING', 'LANGUAGE', 'MOTOR', 'RELATIONAL', 'SOCIAL', 'WM']
@@ -83,14 +86,19 @@ tasks = ['EMOTION', 'GAMBLING', 'LANGUAGE', 'MOTOR', 'RELATIONAL', 'SOCIAL', 'WM
 for task in tasks:
 	# per-subj setup
 	sub = '987983'
+	alltasks_conds = dict()
 
 	# per-task setup; kinda unpythonic, but allows explicit setup of conditions
 	conds = dict()
 	if task == 'EMOTION':
 		conds['fear'] = ['fear.txt'] # names of EV files that should be grouped into this condition
 		conds['neut'] = ['neut.txt']
-		cond_ts = get_cond_timeseries(sub, task, conds)
-		print cond_ts
-
-
-
+	elif task == 'LANGUAGE':
+		conds['story'] = ['story.txt']
+		conds['math'] = ['math.txt']
+	else:
+		continue # do not call get_cond_timepoints unless conditions are specified
+	
+	cond_tps = get_cond_timepoints(sub, task, conds)
+	for cond, tps in cond_tps.items():
+		print '{c}: {s}'.format(c=cond,s=tps)
